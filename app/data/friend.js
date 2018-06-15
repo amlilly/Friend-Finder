79808d5c8d1137604b22f4843608b2646d5e3729
Andrew M. Lilly <amlilly@Andrews-MacBook-Pro-2.local>
2018-06-14 23:48:08 -0500

    added files

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Andrew Lilly',
+    profilePic: 'https://pbs.twimg.com/profile_images/617641588/aaa_400x400.JPG',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
